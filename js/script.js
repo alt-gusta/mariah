c7f1019598d4467e8b70973f39f7ae795b6f8afa
Gustavo William Gonçalves Silva <gustavowilliamgs1@gmail.com>
2024-07-11 09:19:18 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+    function simulateLoading() {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve();
+            }, 3000);
+        });
+    }
+
+    function hideLoadingScreen() {
+        document.getElementById("loading-screen").style.display = "none";
+        document.getElementById("main-content").style.display = "block";
+    }
+
+    function playAudio() {
+        const audio = document.querySelector("audio");
+        audio.play();
+    }
+
+    simulateLoading().then(() => {
+        playAudio();
+        setTimeout(() => {
+            hideLoadingScreen();
+        }, 1500);
+    });
+});
